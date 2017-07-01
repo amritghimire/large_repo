f6e61be99923aba7af30ca61e162db1327eaffe8
Saugat Pachhai <suagatchhetri@outlook.com>
2017-07-01 03:07:01 +0000

    Update train.py

--- train.py
+++ train.py
@@ -56,4 +56,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:984285419943601
+# Comment to update:9842854199436013
