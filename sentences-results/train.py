57534df96c23dfed133022eb13fea85f5f7dad4a
Saugat Pachhai <suagatchhetri@outlook.com>
2019-06-24 00:59:23 +0000

    Document changes in comment

--- sentences-results/train.py
+++ sentences-results/train.py
@@ -60,4 +60,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:37246271153
+# Comment to update:372462711537
