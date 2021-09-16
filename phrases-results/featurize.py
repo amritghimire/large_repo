671b281c298616977b189e9d156b68afb8c8a493
Saugat Pachhai <suagatchhetri@outlook.com>
2021-09-16 10:40:49 +0000

    Document changes in featurize.py

--- phrases-results/featurize.py
+++ phrases-results/featurize.py
@@ -22,4 +22,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:011353972110870530
+# Comment to update:0113539721108705307
