4e099313a1fad0c3b8f998d974f67412ba9675fa
Saugat Pachhai <suagatchhetri@outlook.com>
2021-11-06 10:09:29 +0000

    Fix a typo in train

--- phrases-estimators/train.py
+++ phrases-estimators/train.py
@@ -60,4 +60,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:12486567514
+# Comment to update:124865675141
