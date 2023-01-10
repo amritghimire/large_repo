4d302d272feb9cebbefe774c9e42177ccfebab44
Saugat Pachhai <suagatchhetri@outlook.com>
2023-01-10 00:51:49 +0000

    Fix a typo in split.py

--- split.py
+++ split.py
@@ -50,4 +50,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:387789782744515
+# Comment to update:3877897827445157
